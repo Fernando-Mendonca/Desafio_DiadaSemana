60facfbd2462af0cdc529eab883cbaafd7111a1e
Fernando José Gonçalves Mendonça <nando.jogomend@gmail.com>
2021-10-31 20:13:33 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio_Dia da Semana/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Desafio_Dia_da_Semana
+{
+    partial class frmDiaSemana
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDia = new System.Windows.Forms.Label();
+            this.lblDiaSemana = new System.Windows.Forms.Label();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDia
+            // 
+            this.lblDia.AutoSize = true;
+            this.lblDia.Location = new System.Drawing.Point(76, 33);
+            this.lblDia.Name = "lblDia";
+            this.lblDia.Size = new System.Drawing.Size(23, 13);
+            this.lblDia.TabIndex = 0;
+            this.lblDia.Text = "Dia";
+            // 
+            // lblDiaSemana
+            // 
+            this.lblDiaSemana.AutoSize = true;
+            this.lblDiaSemana.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiaSemana.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblDiaSemana.Location = new System.Drawing.Point(75, 106);
+            this.lblDiaSemana.Name = "lblDiaSemana";
+            this.lblDiaSemana.Size = new System.Drawing.Size(0, 24);
+            this.lblDiaSemana.TabIndex = 1;
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(119, 30);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(61, 20);
+            this.txtDia.TabIndex = 2;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(38, 158);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.TabIndex = 3;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(159, 158);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 4;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // frmDiaSemana
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.lblDiaSemana);
+            this.Controls.Add(this.lblDia);
+            this.Name = "frmDiaSemana";
+            this.Text = "Dia da Semana";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDia;
+        private System.Windows.Forms.Label lblDiaSemana;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ Desafio_Dia da Semana/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio_Dia_da_Semana
+{
+    public partial class frmDiaSemana : Form
+    {
+        public frmDiaSemana()
+        {
+            InitializeComponent();
+        }
+
+        int dia;
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            dia = Convert.ToInt32(txtDia.Text);
+
+            switch (dia)
+            {
+                case 1:
+                    lblDiaSemana.Text = "Domingo";
+                    break;
+                case 2:            
+                    lblDiaSemana.Text = "Segunda Feira";
+                    break;
+                case 3:
+                    lblDiaSemana.Text = "Terça Feira";
+                    break;
+                case 4:
+                    lblDiaSemana.Text = "Quarta Feira";
+                    break;
+                case 5:
+                    lblDiaSemana.Text = "Quinta Feira";
+                    break;
+                case 6:
+                    lblDiaSemana.Text = "Sexta Feira";
+                    break;
+                case 7:
+                    lblDiaSemana.Text = "Sábado";
+                    break;
+
+                default:
+                    lblDiaSemana.Text = "Dia Inválido";
+                    break;
+
+            }
+
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
